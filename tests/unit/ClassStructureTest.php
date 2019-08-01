6e1937e185c8b56c9e0207c340190fb79fa81953
Shawn Bird <shawn.cameron.bird@gmail.com>
2019-08-01 17:37:47 +0200

    Tests: Add 'em
    We start out by making sure that our custom ClassStructure performs the
    way that we expect it to.

---
+++ tests/unit/ClassStructureTest.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types = 1);
+
+use PHPUnit\Framework\TestCase;
+
+use Actindo\Pim\Schema\LoginRequest;
+
+class ClassStructureTest extends TestCase {
+   public function testAccessingValidProperty() {
+      $body = new LoginRequest;
+      $body->setLogin('login_a');
+      $this->assertEquals('login_a', $body->login);
+      $body->login = 'login_b';
+      $this->assertEquals('login_b', $body->login);
+   }
+
+   public function testGettingInvalidProperty() {
+      $this->expectException(Exception::class);
+      $body = new LoginRequest;
+      $x = $body->invalidProperty;
+   }
+
+   public function testSettingInvalidProperty() {
+      $this->expectException(Exception::class);
+      $body = new LoginRequest;
+      $body->invalidProperty = 1;
+   }
+
+   public function testExportToPrimitive() {
+      $body = (new LoginRequest)->setLogin('login1')->setPass('pass1');
+      $obj = $body->toPrimitive();
+      $this->assertEquals((object)[
+         'login' => 'login1',
+         'pass' => 'pass1',
+      ], $obj);
+   }
+
+   public function testExportToJson() {
+      $body = (new LoginRequest)->setLogin('login1')->setPass('pass1');
+      $json = $body->toJson();
+      $this->assertEquals(json_encode([
+         'login' => 'login1',
+         'pass' => 'pass1',
+      ]), $json);
+   }
+
+   public function testSchemaValidationOnExportToPrimitive() {
+      $this->expectException(\Swaggest\JsonSchema\InvalidValue::class);
+      // Login must be a string.
+      $body = (new LoginRequest)->setLogin(123)->setPass('pass');
+      $body->toPrimitive();
+   }
+
+   public function testSchemaValidationOnExportToJson() {
+      $this->expectException(\Swaggest\JsonSchema\InvalidValue::class);
+      $body = (new LoginRequest)->setLogin(123)->setPass('pass');
+      $body->toJson();
+   }
+}
