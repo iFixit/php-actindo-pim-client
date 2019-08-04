1099a2d584730421090b54fc52a727fdf7b15f61
Shawn Bird <shawn.cameron.bird@gmail.com>
2019-08-04 11:55:59 +0200

    Pagination: Make setters return $this
    For easy chaining.

--- src/Pagination.php
+++ src/Pagination.php
@@ -12,18 +12,20 @@ class Pagination {
    private $start = self::DEFAULT_START;
    private $limit = self::DEFAULT_LIMIT;
 
-   public function start(int $n) {
+   public function start(int $n): self {
       if ($n < 0) {
          throw new InvalidPagination($n);
       }
       $this->start = $n;
+      return $this;
    }
 
-   public function limit(int $n) {
+   public function limit(int $n): self {
       if ($n < 1) {
          throw new InvalidPagination($n);
       }
       $this->limit = $n;
+      return $this;
    }
 
    public function getStart(): int {
