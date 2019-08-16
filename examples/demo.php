7a906ea790245ea115df76b4ff0ed3dcb930fd1a
Daniel Beardsley <daniel@ifixit.com>
2019-08-16 14:43:36 -0700

    Example: move demo script to main composer.json
    We don't really need a separate composer.json and lock for this.

--- examples/demo.php
+++ examples/demo.php
@@ -2,7 +2,7 @@
 <?php
 declare(strict_types = 1);
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use Actindo\Pim\JsonRpcClient;
 use Actindo\Pim\Client;
